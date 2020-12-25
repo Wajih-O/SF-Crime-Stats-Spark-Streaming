5fc267c3732d9581f9412498cb82bd6b2b42cd3d
Wajih Ouertani <wajih.ouertani@gmail.com>
2020-12-25 09:24:48 +0100

    features: add kafka_server and ProducerServer

---
+++ src/kafka_server.py
@@ -0,0 +1,24 @@
+from producer_server import ProducerServer
+
+
+def run_kafka_server():
+	# get the data file path
+    input_file = "../data/police-department-calls-for-service.json"
+
+    producer = ProducerServer(
+        input_file=input_file,
+        topic="sf.police_call_for_service",
+        bootstrap_servers="localhost:9092",
+        client_id="call_for_service_producer"
+    )
+
+    return producer
+
+
+def feed():
+    producer = run_kafka_server()
+    producer.generate_data()
+
+
+if __name__ == "__main__":
+    feed()
---
+++ src/producer_server.py
@@ -0,0 +1,52 @@
+import json
+import time
+import logging
+
+from kafka import KafkaProducer, KafkaClient
+from kafka.admin import KafkaAdminClient, NewTopic
+
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+BROKER_URL = "localhost:9092"
+
+def create_topic(topic_name, validate_only=True):
+    """Creates the topic with the given topic name"""
+    if topic_name in KafkaClient(BROKER_URL).topic_partitions:
+        logger.warning('topic already exists!')
+        return
+    response = KafkaAdminClient(bootstrap_servers=BROKER_URL).create_topics(
+       new_topics=[NewTopic(name=topic_name, num_partitions=5, replication_factor=1)],
+       validate_only=validate_only
+    )
+    logger.info(response)
+
+
+class ProducerServer(KafkaProducer):
+
+    def __init__(self, input_file, topic, **kwargs):
+        super().__init__(**kwargs)
+        self.input_file = input_file
+        self.topic = topic
+        create_topic(self.topic)
+
+    def generate_data(self):
+        # as the data file is not a newline-separted json strings but rather a json array
+        with open(self.input_file, 'r') as f:
+            messages_as_dicts = json.loads(f.read())
+
+        for message_dict in messages_as_dicts:
+            message = self.dict_to_binary(message_dict)
+            if message is not None:
+                self.send(self.topic, message)
+            time.sleep(1)
+
+    def dict_to_binary(self, json_dict):
+        """ return the json dict to binary """
+        try:
+            return json.dumps(json_dict).encode('utf-8')
+        except Exception as e:
+            logger.error(f"could not load: {json_str}")
+            return None
